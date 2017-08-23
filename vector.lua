dad0ed7950a898d85b27065b8fdb7cf52365d9d0
Vincent Siles <vinz@appah>
2017-08-23 12:06:36 +0200

    adding some vector (length/sub) functions
    Signed-off-by: Vincent Siles <vinz@appah>

--- vector.lua
+++ vector.lua
@@ -7,6 +7,22 @@ function vector.new(x, y)
     }
 end
 
+function vector.sub(v1, v2)
+    return {
+        x = v1.x - v2.x,
+        y = v1.y - v2.y,
+    }
+end
+
+function vector.length2(v)
+    return (v.x * v.x) + (v.y * v.y)
+end
+
+function vector.length(v)
+    local l2 = vector.length2(v)
+    return math.sqrt(l2)
+end
+
 function vector.eq(v1, v2)
     return (v1.x == v2.x) and (v1.y == v2.y)
 end
